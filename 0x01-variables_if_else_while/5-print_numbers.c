73615bbb855a4c364341e7f8c9c890479c1b9c27
Peter <petrelliomosaiye@gmail.com>
2023-10-20 08:08:34 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = '0';
+		while
+			(a <= '9') {
+				putchar (a);
+				a++;
+			}
+				putchar('\n');
+	return (0);
+}
