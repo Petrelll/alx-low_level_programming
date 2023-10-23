db37e042770f15dcc1b46c973d4b913395ec538c
Peter <petrelliomosaiye@gmail.com>
2023-10-23 15:49:21 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,8 @@ int main(void)
 
 	for (a = 92; a < 99; a++)
 	{
-		printf(", %lu", a);
+		printf(", %lu", c1 + (c2 / 1000000000));
+		printf("%lu", c2 % 1000000000);
 		c1 = c1 + b1;
 		b1 = c1 - b1;
 		c2 = c2 + b2;
