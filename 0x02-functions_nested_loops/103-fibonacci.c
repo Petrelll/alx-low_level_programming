8f0b1a34b5d833a9daa1f7dfd3199ab39d4a2f4c
Peter <petrelliomosaiye@gmail.com>
2023-10-23 14:40:26 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued terms
+ *
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	long int a, y = 1, z = 2, sum = 0, tsum = 0;
+
+	for (a = 0; a < 49; a++)
+	{
+		if ((z % 2 == 0) && (z <= 4000000))
+		{
+			tsum = tsum + z;
+		}
+		sum = y + z;
+		y = z;
+		z = sum;
+	}
+	return (0);
+}
