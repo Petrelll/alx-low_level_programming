6cc20bb0e26b66d84967d07c21d60bff470a5265
Peter <petrelliomosaiye@gmail.com>
2023-10-23 11:12:06 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,11 @@
 
 
 /**
- * Add - Entry point
- *
- * Description: A function that adds two integers and
- * returns the result.
+ * add - Entry point
  *
  * @n1: Integer one
  * @n2: Integer two
-
+ *
  * Return: The sum
  */
 
@@ -17,7 +14,6 @@
 int add(int n1, int n2)
 {
 	int sum;
-
 
 	sum = n1 + n2;
 	return (sum);
