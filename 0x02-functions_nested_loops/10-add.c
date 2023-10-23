89970a90f74f05cb2f8ab2473620253d7c532a5a
Peter <petrelliomosaiye@gmail.com>
2023-10-23 10:30:17 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * Add - Entry point
+ *
+ * Description: A function that adds two integers and
+ * returns the result.
+ *
+ * @n1: Integer one
+ * @n2: Integer two
+
+ * Return: The sum
+ */
+
+
+int add(int n1, int n2)
+{
+	int sum;
+
+
+	sum = n1 + n2;
+	return (sum);
+}
